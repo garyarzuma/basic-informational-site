2f2f240bfa83117dc119a75a42fd5cf3a07a0722
Gary <garyarzumanyan@gmail.com>
2021-02-07 12:18:17 -0800

    Rewrote index.js using Express and app.get() methods

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 const http = require("http");
 const { URL, URLSearchParams } = require("url");
 const fs = require("fs");
+const express = require("express");
+const app = express();
+
+app.get("/about", function (request, response) {
+  const filename = "./about.html";
+
+  fs.readFile(filename, function (err, data) {
+    response.write(data);
+    return response.end();
+  });
+});
 
 //create a server object:
-http
-  .createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/html" });
+app.get("/", function (request, response) {
+  const filename = "./index.html";
 
-    const baseURL = "https://" + request.headers.host;
-    const myURL = new URL(request.url, baseURL);
-    const filename =
-      myURL.pathname === "/" ? "./index.html" : "." + myURL.pathname + ".html";
+  fs.readFile(filename, function (err, data) {
+    response.write(data);
+    return response.end();
+  });
+});
 
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        return fs.readFile("./404.html", function (err, data) {
-          response.writeHead(404, { "Content-Type": "text/html" });
-          return response.end(data);
-        });
-      }
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(data);
-      return response.end();
-    });
-  })
-  .listen(8080); //the server object listens on port 8080
+app.get("/contact-me", function (request, response) {
+  const filename = "./contact-me.html";
+
+  fs.readFile(filename, function (err, data) {
+    response.write(data);
+    return response.end();
+  });
+});
+
+app.get("*", function (request, response) {
+  const filename = "./404.html";
+
+  fs.readFile(filename, function (err, data) {
+    response.write(data);
+    return response.end();
+  });
+});
+
+app.listen(8080); //the server object listens on port 8080
