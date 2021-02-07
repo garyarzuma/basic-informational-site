d40af83a876168482727a8ba833c809d03febbfb
Gary <garyarzumanyan@gmail.com>
2021-02-06 22:51:42 -0800

    Added filename loading function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var http = require("http");
+const http = require("http");
 const { URL, URLSearchParams } = require("url");
+const fs = require("fs");
 
 //create a server object:
 http
@@ -8,9 +9,18 @@ http
 
     const baseURL = "https://" + request.headers.host;
     const myURL = new URL(request.url, baseURL);
-    const text = myURL.searchParams.get("year");
+    const filename = "." + myURL.pathname;
 
-    response.write("<p>" + text + "</p>"); //write a response to the client */
-    response.end(); //end the response */
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        response.writeHead(404, { "Content-Type": "text/html" });
+        return response.end("404 Not Found");
+      }
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(data);
+      return response.end();
+    });
+
+    /*  response.write("<p>" + text + "</p>"); //write a response to the client */
   })
   .listen(8080); //the server object listens on port 8080
